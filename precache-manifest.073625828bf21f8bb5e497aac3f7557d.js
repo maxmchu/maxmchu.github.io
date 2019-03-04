98d01155f706dd0cec875de766597a8dda935b0f
Max Chu <maxmchu@gmail.com>
2019-03-03 22:54:26 -0500

    Updates

--- precache-manifest.073625828bf21f8bb5e497aac3f7557d.js
+++ precache-manifest.073625828bf21f8bb5e497aac3f7557d.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "55eaebd7b6d3850f16af",
+    "revision": "fdd09422b332efc8ba6a",
     "url": "/static/css/main.05c6ebd5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "40730f4a93b410ff8ef9",
-    "url": "/static/js/2.40730f4a.chunk.js"
+    "revision": "aca9df12912a13181f5c",
+    "url": "/static/js/2.aca9df12.chunk.js"
   },
   {
     "revision": "cf528d80b3f0fb5c1728cc50e7e77759",
@@ -44,20 +44,24 @@ self.__precacheManifest = [
     "url": "/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "55eaebd7b6d3850f16af",
-    "url": "/static/js/main.55eaebd7.chunk.js"
-  },
-  {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/static/media/brand-icons.e8c322de.woff2"
+    "revision": "fdd09422b332efc8ba6a",
+    "url": "/static/js/main.fdd09422.chunk.js"
   },
   {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/static/media/icons.faff9214.woff"
   },
   {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
@@ -72,10 +76,6 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/static/media/brand-icons.13db00b7.eot"
-  },
-  {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/static/media/outline-icons.82f60bd0.svg"
   },
@@ -84,11 +84,11 @@ self.__precacheManifest = [
     "url": "/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "40730f4a93b410ff8ef9",
+    "revision": "aca9df12912a13181f5c",
     "url": "/static/css/2.63902fe0.chunk.css"
   },
   {
-    "revision": "48def3470c2f0dac587e159d6b334b4d",
+    "revision": "99dc1609252776ec3738680ffe5f3177",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.815fe6dc1e54193dd6ff59f9d7774172.js"
+  "/precache-manifest.073625828bf21f8bb5e497aac3f7557d.js"
 );
 
 workbox.clientsClaim();
